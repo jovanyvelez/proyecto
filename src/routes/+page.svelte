9acf78cb429a59e7271f3476646a68563805c75f
Jovany <jovany.velez@gmail.com>
2024-08-15 16:07:29 -0500

    version sam

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Hola soy estudiante de 11 </h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Micomponente from "./Micomponente..svelte";
+
+    let color = "brown";
+
+    function cambia() {
+        if(color === "brown") {
+            color = "red";
+        } else if(color === "red") {
+            color = "brown";
+        }
+    }
+</script>
+
+<h2>
+    Habla de mi pagina principla
+</h2>
+
+<h1>
+    Este es un H1 de la pagina principal
+</h1>
+<Micomponente mensajito="Melina" color="blue"/>
+
+<Micomponente mensajito="Hola" color={color} />
+
+<Micomponente mensajito="Adios" color="purple" />  
+
+<Micomponente mensajito="Como estas" color="yellow" />
+
+<button onclick={cambia}>Melina</button>
---
+++ src/routes/Micomponente..svelte
@@ -0,0 +1,14 @@
+<script>
+    let {mensajito, color} = $props()
+</script>
+
+
+<h1>
+    Este es el componente <span style="color: {color}">{mensajito}</span>
+</h1>
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
